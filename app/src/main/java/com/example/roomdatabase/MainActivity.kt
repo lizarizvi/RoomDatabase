96b5b18da55651300472e8ccc05bc71072de8389
LIZA RIZVI <lizarizvi333@gmail.com>
2023-07-18 21:42:22 +0530

    Adding records in table

--- app/src/main/java/com/example/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabase/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.roomdatabase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
+import com.example.roomdatabase.databinding.ActivityMainBinding
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+
+        val employeeDao = (application as EmployeeApp).db.employeeDao()
+        binding?.btnAddRecord?.setOnClickListener {
+            addRecord(employeeDao)
+        }
+    }
+
+    fun addRecord(employeeDao: EmployeeDao){
+        val name = binding?.etName?.text.toString()
+        val email = binding?.etEmail?.text.toString()
+
+        if (name.isNotEmpty() && email.isNotEmpty()){
+            lifecycleScope.launch{
+                employeeDao.insert(EmployeeEntity(name=name, email=email))
+                Toast.makeText(applicationContext, "Record saved.", Toast.LENGTH_SHORT).show()
+                binding?.etName?.text?.clear()
+                binding?.etEmail?.text?.clear()
+            }
+        }else{
+            Toast.makeText(applicationContext, "Enter name and email", Toast.LENGTH_SHORT).show()
+
+        }
     }
 }
