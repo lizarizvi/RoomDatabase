696dae9e2cce99c1440cf67014bd3002344bf619
LIZA RIZVI <lizarizvi333@gmail.com>
2023-07-20 00:29:55 +0530

    finalizing the room database with no errors

--- app/src/main/java/com/example/roomdatabase/MainActivity.kt
+++ app/src/main/java/com/example/roomdatabase/MainActivity.kt
@@ -2,16 +2,15 @@ package com.example.roomdatabase
 
 import android.app.AlertDialog
 import android.app.Dialog
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.R
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.roomdatabase.databinding.ActivityMainBinding
 import com.example.roomdatabase.databinding.DialogUpdateBinding
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -76,14 +75,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun updateRecord(id: Int, employeeDao: EmployeeDao){
-        val updateDialog = Dialog(this, R.style.Theme_AppCompat_Dialog)
+        val updateDialog = Dialog(this, R.style.ThemeOverlay_AppCompat_Dialog_Alert)
         updateDialog.setCancelable(false)
         val binding = DialogUpdateBinding.inflate(layoutInflater)
         updateDialog.setContentView(binding.root)
         lifecycleScope.launch{
             employeeDao.fetchAllEmployeeById(id).collect{
-                binding.etName.setText(it.name)
-                binding.etEmail.setText(it.email)
+
+                if (it != null){
+                    binding.etName.setText(it.name)
+                    binding.etEmail.setText(it.email)
+                }
+
             }
         }
         binding.btnUpdate.setOnClickListener {
@@ -110,6 +113,16 @@ class MainActivity : AppCompatActivity() {
     private fun deleteRecord(id: Int, employeeDao: EmployeeDao){
         val builder = AlertDialog.Builder(this)
         builder.setTitle("Delete Record")
+        builder.setIcon(android.R.drawable.ic_dialog_alert)
+
+        lifecycleScope.launch {
+            employeeDao.fetchAllEmployeeById(id).collect{
+                if (it != null){
+                    builder.setMessage("Are you sure you want to delete ${it.name}")
+                }
+            }
+        }
+
         builder.setPositiveButton("YES"){
             dialogInterface,_->
             lifecycleScope.launch{
@@ -118,6 +131,7 @@ class MainActivity : AppCompatActivity() {
             }
             dialogInterface.dismiss()
         }
+
         builder.setNegativeButton("NO"){
                 dialogInterface,_->
             dialogInterface.dismiss()
