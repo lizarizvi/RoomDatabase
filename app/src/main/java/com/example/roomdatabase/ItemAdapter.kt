0459b689b774300429d3a8b9795e22cc26c51c39
LIZA RIZVI <lizarizvi333@gmail.com>
2023-07-19 13:53:09 +0530

    ItemAdapter

---
+++ app/src/main/java/com/example/roomdatabase/ItemAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.roomdatabase
+
+import android.animation.ValueAnimator.AnimatorUpdateListener
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.roomdatabase.databinding.ItemsRowBinding
+
+class ItemAdapter(private var items: ArrayList<EmployeeEntity>, private val updateListener: (id:Int)->Unit,
+                  private val deleteListener: (id:Int)->Unit) :RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
+
+    class ViewHolder(binding: ItemsRowBinding): RecyclerView.ViewHolder(binding.root){
+        val llItem = binding.llItem
+        val tvName = binding.tvName
+        val tvEmail = binding.tvEmail
+        val ivEdit = binding.ivEdit
+        val ivDelete = binding.ivDelete
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemsRowBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val context = holder.itemView.context
+        val item = items[position]
+
+        holder.tvName.text = item.name
+        holder.tvEmail.text = item.email
+
+        if (position % 2 == 0){
+            holder.llItem.setBackgroundColor(ContextCompat.getColor(holder.itemView.context,R.color.lightGrey))
+        }else{
+            holder.llItem.setBackgroundColor(ContextCompat.getColor(holder.itemView.context,R.color.white))
+        }
+
+        holder.ivEdit.setOnClickListener {
+            updateListener.invoke(item.id)
+        }
+        holder.ivDelete.setOnClickListener {
+            deleteListener.invoke(item.id)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
